147b3320fa687d71e813b3e630fcca04af0185cf
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-06 00:24:42 +0100

    tried creating a python dictionary of categories and subcategories. I didn't finish and will most likely delete it later

---
+++ classifiedads/classifiedads/category.py
@@ -0,0 +1,16 @@
+
+AD_CATEGORY_DICT = [
+    {"name": "Vehicles",       "id": "336"},
+    {"name": "Services",       "id": "462"},
+    {"name": "For Rent",       "id": "468"},
+    {"name": "Real Estate",    "id": "18"},
+    {"name": "Community",      "id": "5"},
+    {"name": "Pets",           "id": "17"},
+    {"name": "Jobs",           "id": "15"},
+    {"name": "Items for Sale", "id": "4"},
+    {"name": ""}
+]
+
+AD_SUBCATEGORY_DICT = {
+
+}
