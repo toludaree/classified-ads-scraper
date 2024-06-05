947f32af6908114601d88955b7106f06c534f4a9
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-05 17:47:21 +0100

    copied implementation from udemy course

--- classifiedads/classifiedads/items.py
+++ classifiedads/classifiedads/items.py
@@ -4,9 +4,21 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import Join, MapCompose
+from w3lib.html import replace_tags
 
+
+def description_in(d):
+    return replace_tags(d, "\n")
+
+def clean(d):
+    return d[0].strip()
 
 class ClassifiedadsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field(output_processor=Join())
+    city = scrapy.Field(output_processor=Join())
+    phone = scrapy.Field(output_processor=Join())
+    zip = scrapy.Field(output_processor=clean)
+    description = scrapy.Field(input_processor=MapCompose(description_in),
+                               output_processor=clean)
+    price = scrapy.Field(output_processor=Join())
--- classifiedads/classifiedads/spiders/ads.py
+++ classifiedads/classifiedads/spiders/ads.py
@@ -1,10 +1,33 @@
-import scrapy
+
+from classifiedads.items import ClassifiedadsItem
+from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class AdsSpider(scrapy.Spider):
+class AdsSpider(CrawlSpider):
     name = "ads"
     allowed_domains = ["classifiedads.com"]
-    start_urls = ["https://classifiedads.com"]
+    start_urls = ["https://www.classifiedads.com/search.php?keywords=&cid=627&lid=gx2339354&lname=Earth&from=c"]
 
-    def parse(self, response):
-        pass
+    rules = (
+        Rule(
+            LinkExtractor(restrict_xpaths="//div[@class='resultitem']"),
+            callback="parse", follow=True
+        ),
+        Rule(
+            LinkExtractor(restrict_xpaths="//a[@class='npage']"),
+            callback="parse", follow=True
+        ),
+    )
+
+    def parse(self, response): 
+        item = ItemLoader(item=ClassifiedadsItem(), response=response, selector=response)
+        item.add_xpath("title", "//h1[contains(@class, 'itemtitle')]/text()")
+        item.add_xpath("city", "//div[span='City:']/span[@class='last']/text()")
+        item.add_xpath("phone", "//div[span='Phone:']/span[@class='last']/text()")
+        item.add_xpath("zip", "//div[span='Zip:']/span[@class='last']/text()")
+        item.add_xpath("description", "//div[@class='description']")   # inner element issue, extracting the html element instead
+        item.add_xpath("price", "//div[span='Price:']/span[@class='last']/strong/text()")
+
+        yield item.load_item()
