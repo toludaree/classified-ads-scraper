e06529e29b8a47026947c0a560abaa8f14a137f4
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-06 10:15:00 +0100

    removed id field: redundant

--- classifiedads/classifiedads/items.py
+++ classifiedads/classifiedads/items.py
@@ -29,11 +29,9 @@ class ClassifiedadsItem(Item):
 
 class AdSubCategoriesItem(Item):
     name = Field()
-    id = Field()
     url = Field()
 
 class AdCategoriesItem(Item):
     name = Field()
-    id = Field()
     url = Field()
     subcategories = AdSubCategoriesItem()
