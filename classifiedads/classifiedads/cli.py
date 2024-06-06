a717554c5f7298854da937e1220077e5c29e345b
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-06 23:24:16 +0100

    - added help attributes to all the parsers - added 2 more arguments to the categories subparser

--- classifiedads/classifiedads/cli.py
+++ classifiedads/classifiedads/cli.py
@@ -2,15 +2,16 @@ from argparse import ArgumentParser
 
 
 parser = ArgumentParser(description="Scraper for ClassifiedAds.com")
-subparser = ArgumentParser.add_subparsers(parser, help="subparsers help")
+subparser = ArgumentParser.add_subparsers(parser)
 
-scrape_subparser = subparser.add_parser("scrape", help="scrape help")
-scrape_subparser.add_argument("id", help="id help")
-scrape_subparser.add_argument("-f", "--format", help="format help")
+scrape_subparser = subparser.add_parser("scrape", help="Scrape a particular category or subcategory")
+scrape_subparser.add_argument("id", help="Category or Subcategory ID. Run ... to confirm ID")
+scrape_subparser.add_argument("-f", "--format", help="file format to save results to. Choose one of json, xml, csv")
 
-categories_subparser = subparser.add_parser("categories", help="categories help")
-categories_subparser.add_argument("-a", "--all", help="all help")
+categories_subparser = subparser.add_parser("categories", help="List the IDs and Names of categories and/or subcategories")
+category_group = categories_subparser.add_mutually_exclusive_group()
+category_group.add_argument("-a", "--all", help="List all category and subcategory names and their ids", action="store_true")
+category_group.add_argument("-c", "--only-category", help="List all category names and their ids", action="store_true")
+category_group.add_argument("-s", "--only-subcategory", help="List all subcategory names and their ids", action="store_true")
 
 parser.parse_args()
-
-print("Done")
