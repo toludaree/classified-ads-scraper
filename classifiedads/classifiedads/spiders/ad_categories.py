bb222703730966bcfd80ac013db210aa11af5d22
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-06 10:25:31 +0100

    tried implementing the relationship between AdCategoryItem and AdSubCategoryItem myself

--- classifiedads/classifiedads/spiders/ad_categories.py
+++ classifiedads/classifiedads/spiders/ad_categories.py
@@ -1,4 +1,6 @@
 import scrapy
+from itemloaders import ItemLoader
+from classifiedads.items import AdCategoriesItem, AdSubCategoriesItem
 
 
 class AdCategoriesSpider(scrapy.Spider):
@@ -7,4 +9,19 @@ class AdCategoriesSpider(scrapy.Spider):
     start_urls = ["https://classifiedads.com"]
 
     def parse(self, response):
-        pass
+        gallery = response.xpath("//div[contains(@class, 'cat0-')]")
+        for category in gallery:
+            item = ItemLoader(item=AdCategoriesItem(),
+                              response=response, selector=category)
+
+            item.add_xpath("name", "./div/a/text()")
+            item.add_xpath("url", "./div/a/@href")
+            item.add_value("subcategories", )
+
+            yield item.load_item()
+            # item_sub = ItemLoader(item=AdSubCategoriesItem(), response=response, selector=response)
+
+            # item_sub.load_item
+
+    def parse_subcategory(self, response):
+        ...
