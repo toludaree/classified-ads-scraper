5ced2ce8bdd1cd35898598b13211740cab401aef
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-06-07 19:46:01 +0100

    - revamped the model - updated my parsing logic to account for all and only_category cases - improved some helper functions

--- classifiedads/classifiedads/cli/cli.py
+++ classifiedads/classifiedads/cli/cli.py
@@ -1,20 +1,22 @@
 import json
 
 
-def list_only_category():
+ROW_LENGTH = 21
+
+
+def list_all():
     categories = get_categories()
-    id_and_name = parse_categories(categories)
+    categories_parsed = ...
 
-    length = 21
+def list_only_category() -> str:
+    categories = get_categories()
+    id_and_name = parse(categories, only_category=True)
 
-    headers = [get_general_header("Categories"),
-               get_table_header(length)]
-    # general_header = get_general_header("Categories")
-    # table_header = get_table_header(length)
-    table_rows = get_table_rows(id_and_name, length)
-    headers.extend(table_rows)
-
-    return "\n".join(headers)
+    title = get_title("Categories")
+    table_header = get_table_header()
+    table_rows = get_table_rows(id_and_name)
+    
+    return "\n".join([title, table_header] + table_rows)
 
 
 def get_categories() -> list[dict]:
@@ -22,15 +24,24 @@ def get_categories() -> list[dict]:
         categories = json.load(f)
         return categories
     
-def parse_categories(cs:list[dict]) -> list[tuple]:
-    return list(map(parse_category, cs))
+def parse(cs:list[dict], only_category=False):
+    if only_category:
+        return list(map(parse_one_category, cs))
+    return list(map(parse_one, cs))
 
-def parse_category(c:dict) -> tuple:
+def parse_one(c:dict) -> tuple:
+    if "subcategories" in c:
+        return (c["id"], c["name"],
+                list(map(parse_one, c["subcategories"])))
+    else:
+        return (c["id"], c["name"])
+
+def parse_one_category(c):
     return (c["id"], c["name"])
 
-def get_general_header(name:str) -> str:
-    rule = "="*(len(name) + 1)
-    return name + "\n" + rule
+def get_title(name:str) -> str:
+    ruler = "="*(len(name) + 1)
+    return name + "\n" + ruler
 
 def get_category_header(name:str, row_length:int) -> str:
     rule = "-"*(len(name) + 1)
@@ -38,24 +49,25 @@ def get_category_header(name:str, row_length:int) -> str:
             "\n" +
             rule.ljust(row_length))
 
-def get_table_header(row_length:int) -> str:
-    header = (" ID" + " | " + "Name").ljust(row_length)
-    rule = "-"*row_length
-    return header + "\n" + rule
+def get_table_header() -> str:
+    column_names = (" ID" + " | " + "Name").ljust(ROW_LENGTH)
+    ruler = "-"*ROW_LENGTH
+    header = column_names + "\n" + ruler
+    return header
 
-def get_table_rows(val:list[tuple], length:int) -> list[str]:
-    return list(map(lambda val: get_table_row(val, length), val))
+def get_table_rows(val:list[tuple]) -> list[str]:
+    return list(map(get_table_row, val))
 
-def get_table_row(val:tuple, row_length:int) -> str:
+def get_table_row(val:tuple) -> str:
     content = val[0].ljust(3) + " | " + val[1]
-    return content.ljust(row_length)
+    return content.ljust(ROW_LENGTH)
 
 
 """
 Categories
 ===========
  ID | Name
----------------------
+--------------------
 336 | Vehicles
 462 | Services
 468 | For Rent
@@ -63,30 +75,48 @@ Categories
   5 | Community
  17 | Pets
  15 | Jobs
-  4 | Items for Sale
+  4 | Items for
+    | Sale
 
-Sub-Categories
-===============
-Vehicles                            Services 
-.........                           .........
-ID  | Name                          ID  | Name
----------------------------------   -----------------------------
- 73 | Aircraft                      467 | Automotive Services
-462 | Automotive Items and Parts     83 | Beauty & Salon Services
+All
+====
+(336) Vehicles       
+...............      
+ID  | Name           
+---------------------
+ 73 | Aircraft       
+462 | Automotive     
+    | Items and      
+    | Parts          
 
-For Rent                            Real Estate
-.........                           ............
-ID  | Name                          ID  | Name
------------------------             -----------------------------
-299 | Apartments                    301 | Commercial Real Estate
-302 | Commercial Lease              312 | Condos For Sale
+(468) For Rent       
+...............      
+ID  | Name           
+---------------------
+299 | Apartments     
+302 | Commercial          
+    | Lease
 ...
-"""
 
-"""
-Categories
-- title
-- content
+    
+All
+====
+(336) Vehicles          (462) Services
+...............         ...............
+ID  | Name              ID  | Name
+---------------------   ---------------------
+ 73 | Aircraft          467 | Automotive
+462 | Automotive            | Services
+    | Items and         83  | Beauty & Salon
+    | Parts                 | Services
 
-title 
+(468) For Rent          (18) Real Estate
+...............         .................
+ID  | Name              ID  | Name
+---------------------   ---------------------
+299 | Apartments        301 | Commercial
+302 | Commercial            | Real Estate     
+    | Lease             312 | Condos For
+                            | Sale
+...
 """
